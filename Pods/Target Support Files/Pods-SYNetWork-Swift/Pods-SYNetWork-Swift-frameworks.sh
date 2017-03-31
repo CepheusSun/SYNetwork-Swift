0a676c997ff5199010a552629403476a61161476
CepheusSun <cd_sunyang@163.com>
2017-03-31 15:25:52 +0800

    移除rxcocoa

--- Pods/Target Support Files/Pods-SYNetWork-Swift/Pods-SYNetWork-Swift-frameworks.sh
+++ Pods/Target Support Files/Pods-SYNetWork-Swift/Pods-SYNetWork-Swift-frameworks.sh
@@ -90,12 +90,10 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
